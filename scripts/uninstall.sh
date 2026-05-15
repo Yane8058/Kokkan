f2322c0e7c0422039e39187402c28545183ae198
Enrico Iannone <156577430+Yane8058@users.noreply.github.com>
2026-05-15 09:26:22 +0100

    Update uninstall.sh

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -1,1 +1,17 @@
-# uninstall.sh
+#!/bin/bash
+
+set -e
+
+echo "[*] Removing Kokkan..."
+
+sudo systemctl stop kokkan.timer
+sudo systemctl disable kokkan.timer
+
+sudo rm /etc/systemd/system/kokkan.timer
+sudo rm /etc/systemd/system/kokkan.service
+
+sudo systemctl daemon-reload
+
+sudo rm -rf /opt/kokkan
+
+echo "[OK] Kokkan removed"
