eea937eaf9c7039a3a71609f79e39cc8c018635c
Enrico Iannone <156577430+Yane8058@users.noreply.github.com>
2026-05-15 09:25:34 +0100

    Add installation script for Kokkan

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,1 +1,25 @@
-# install.sh
+#!/bin/bash
+
+set -e
+
+echo "[*] Installing Kokkan..."
+
+# make directory
+sudo mkdir -p /opt/kokkan
+
+# copy file
+sudo cp -r ../* /opt/kokkan/
+
+# copy systemd files
+sudo cp ../systemd/kokkan.service /etc/systemd/system/
+sudo cp ../systemd/kokkan.timer /etc/systemd/system/
+
+# reload systemd
+sudo systemctl daemon-reexec
+sudo systemctl daemon-reload
+
+# enable timer
+sudo systemctl enable kokkan.timer
+sudo systemctl start kokkan.timer
+
+echo "[OK] Kokkan installed and running"
